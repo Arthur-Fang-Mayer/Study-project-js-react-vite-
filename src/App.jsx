a1a5f07bcd270e8fe8f58aa27b7408903875c49d
Arthur <arthurfangmaier@gmail.com>
2025-03-21 00:09:38 +0200

    Added states

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,35 @@ import Image from "./components/image";
 import logo from "./img/logo.png"
 
 class App extends React.Component {
-    helpText = "Help text!"
-    inputClick(){console.log("Clicked")}
+    constructor(props){
+        super(props)
+        this.state = {
+            helpText : "Help text!",
+            userData : ""
+        }
+
+        this.inputClick = this.inputClick.bind(this)
+    }
+    
+    inputClick(){
+        this.setState({helpText : "Changed"})
+        console.log("Clicked")
+    }
     mouseOver(){console.log("Mouse Over")}
 
     render() { 
         return(
             <div className="name">
                 <Header title="This is header title"/>
-                <h1>{this.helpText}</h1>
+                <h1>{this.state.helpText}</h1>
+                <h2>{this.state.userData}</h2>
                 <input 
-                    placeholder={this.helpText}
+                    placeholder={this.state.helpText}
                     onClick={this.inputClick}
                     onMouseEnter={this.mouseOver}
+                    onChange = {event => this.setState({userData : event.target.value})}
                 />
+                
                 <p>{this.helpText == "Help text!" ? "Yes" : "No"}</p>
                 <Image image={logo}/>
                 <Footer />
